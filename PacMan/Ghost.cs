03a9327be87a22d45529d107125f7aa53fe3248e
Mikah Kainen <mikah@mltd.com>
2021-03-29 14:16:58 -0700

    Added Ghost

---
+++ PacMan/Ghost.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static PacMan.Enum;
+
+namespace PacMan
+{
+    public class Ghost : AnimationSprite
+    {
+        private Pacman targetPac;
+        private List<Tile> walls;
+        private List<Ghost> otherGhosts;
+        private float speedPerSecond;
+        private int iterationsPerUpdate;
+        private ScreenManager screenManager;
+        private InputManager inputManager;
+
+        public Ghost(Texture2D tex, Color tint, Vector2 pos, Vector2 scale, List<AnimationFrame> frames, TimeSpan timeBetweenFrames, Pacman targetPac, List<Tile> walls, List<Ghost> otherGhosts, float speedPerUpdate, int iterationsPerUpdate, ScreenManager screenManager, InputManager inputManager)
+            :base(tex, tint, pos, scale, frames, timeBetweenFrames)
+        {
+            this.targetPac = targetPac;
+            this.walls = walls;
+            this.otherGhosts = otherGhosts;
+            this.speedPerSecond = speedPerSecond;
+            this.iterationsPerUpdate = iterationsPerUpdate;
+            this.screenManager = screenManager;
+            this.inputManager = inputManager;
+        }
+
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+        }
+
+
+        private Directions CalculateDirection()
+        {
+            return Directions.None;
+        }
+    }
+}
---
+++ PacMan/IMoving.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static PacMan.Enum;
+
+namespace PacMan
+{
+    public interface IMoving
+    {
+    //    abstract int speedPerSecond { get; set; }
+    //    abstract int iterationsPerUpdate { get; set; }
+
+
+    //    float Move(Directions currentDirection, Vector2 Pos)
+    //    {
+    //        for (int x = 0; x < iterationsPerUpdate; x++)
+    //        {
+    //            //for (int i = PreviousPositions.Length - 1; i > 0; i--)
+    //            //{
+    //            //    PreviousPositions[i] = PreviousPositions[i - 1];
+    //            //}
+    //            //PreviousPositions[0] = Pos;
+    //            switch (currentDirection)
+    //            {
+    //                case Directions.Up:
+    //                    Pos.Y -= speed;
+    //                    Rotation = 3 * (float)Math.PI / 2;
+    //                    break;
+
+    //                case Directions.Down:
+    //                    Pos.Y += speed;
+    //                    Rotation = (float)Math.PI / 2;
+    //                    break;
+
+    //                case Directions.Left:
+    //                    Pos.X -= speed;
+    //                    Rotation = (float)Math.PI;
+    //                    break;
+
+    //                case Directions.Right:
+    //                    Pos.X += speed;
+    //                    Rotation = 0;
+    //                    break;
+
+    //                default:
+    //                    base.currentIndex = 1;
+    //                    break;
+    //            }
+    //            if (HitBorder())
+    //            {
+    //                CurrentDirection = Directions.None;
+    //            }
+    //        }
+    //    }
+        
+    }
+}
