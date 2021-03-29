5dd281ccda151228feb9f464129b1698334cd5fc
Mikah <mikah@mltd.com>
2021-03-29 15:45:48 -0700

    Added Ghost

--- PacMan/Ghost.cs
+++ PacMan/Ghost.cs
@@ -9,30 +9,61 @@ namespace PacMan
 {
     public class Ghost : AnimationSprite
     {
-        private Pacman targetPac;
-        private List<Tile> walls;
-        private List<Ghost> otherGhosts;
-        private float speedPerSecond;
+        //private Pacman targetPac;
+        //private List<Tile> walls;
+        //private List<Ghost> otherGhosts;
+        private float speedPerUpdate;
         private int iterationsPerUpdate;
         private ScreenManager screenManager;
         private InputManager inputManager;
+        public Directions CurrentDirection { get; set; }
 
-        public Ghost(Texture2D tex, Color tint, Vector2 pos, Vector2 scale, List<AnimationFrame> frames, TimeSpan timeBetweenFrames, Pacman targetPac, List<Tile> walls, List<Ghost> otherGhosts, float speedPerUpdate, int iterationsPerUpdate, ScreenManager screenManager, InputManager inputManager)
-            :base(tex, tint, pos, scale, frames, timeBetweenFrames)
+        public Ghost(Texture2D tex, Color tint, Vector2 pos, Vector2 scale, List<AnimationFrame> frames, float speedPerUpdate, int iterationsPerUpdate, ScreenManager screenManager, InputManager inputManager)
+            :base(tex, tint, pos, scale, frames, TimeSpan.FromMilliseconds(100))
         {
-            this.targetPac = targetPac;
-            this.walls = walls;
-            this.otherGhosts = otherGhosts;
-            this.speedPerSecond = speedPerSecond;
+            //this.targetPac = targetPac;
+            //this.walls = walls;
+            //this.otherGhosts = otherGhosts;
+            this.speedPerUpdate = speedPerUpdate;
             this.iterationsPerUpdate = iterationsPerUpdate;
             this.screenManager = screenManager;
             this.inputManager = inputManager;
+            CurrentDirection = Directions.None;
         }
 
 
         public override void Update(GameTime gameTime)
         {
-            base.Update(gameTime);
+            //base.Update(gameTime);
+            for (int i = 0; i < iterationsPerUpdate; i ++)
+            {
+                switch (CurrentDirection)
+                {
+                    case Directions.Up:
+                        Pos.Y -= speedPerUpdate;
+                        currentIndex = 0;
+                        break;
+
+                    case Directions.Down:
+                        Pos.Y += speedPerUpdate;
+                        currentIndex = 1;
+                        break;
+
+                    case Directions.Right:
+                        Pos.X += speedPerUpdate;
+                        currentIndex = 2;
+                        break;
+
+                    case Directions.Left:
+                        Pos.X -= speedPerUpdate;
+                        currentIndex = 3;
+                        break;
+
+                    default:
+
+                        break;
+                }
+            }
         }
 
         public override void Draw(SpriteBatch spriteBatch)
